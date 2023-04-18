db66a945e27d4c2298345d4f326ec29a65f4fb27
Ryan Ostrander <ryan08@uw.edu>
2023-04-18 15:00:19 -0700

    added class for algorithm development

---
+++ algorithm_class.py
@@ -0,0 +1,10 @@
+
+class Range_est():
+    
+    # if self.data['state_run']==1:
+    #     initial_SOC = self.data['soc']
+    #     initial_distance = self.data['tripDistance']
+    #     initial_time = self.data['time']
+
+    def overall_avg(data):
+        """"""
